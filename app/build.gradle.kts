c0f72a7bf61d5479727e19d0ff8e0cc347d4cdff
yamidev52 <sergioyamilng2005@gmail.com>
2025-10-13 19:34:49 -0600

    Gradle actualizado

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.yamidev.drinkfinder"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.yamidev.drinkfinder"
         minSdk = 30
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
