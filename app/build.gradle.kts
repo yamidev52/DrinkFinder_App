eab25680459cd90f4fb03478947f77f0b3c52373
yamidev52 <sergioyamilng2005@gmail.com>
2025-11-28 22:39:25 -0600

    Funcion Mapas Eliminada

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -80,5 +80,6 @@ dependencies {
     implementation("com.google.android.gms:play-services-maps:18.2.0")
     implementation("com.google.android.gms:play-services-location:21.2.0")
     implementation ("com.google.android.libraries.places:places:3.4.0")
+    //implementation("com.google.android.gms:play-services-maps-renderer:18.0.0")
 
 }
